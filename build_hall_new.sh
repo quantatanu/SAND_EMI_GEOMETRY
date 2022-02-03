8d9aaf9461296f79c26fd0c661b8075dd1e5d0e8
quantatanu <atanu.quanta@gmail.com>
2022-02-03 21:37:16 +0530

    RPC created for the first time

--- build_hall_new.sh
+++ build_hall_new.sh
@@ -17,7 +17,215 @@ fi
 #  3. LAr + TMS     (+ SAND)
 
 curdir=$(pwd);
-outdir="${curdir}/OUTPUT";
+outdir="${curdir}/OUTPUT/GDML/";
+
+
+reset;
+if [ $option = "-C" -o "-compile" ]
+then
+    echo "You have chosen to compile, compiling...."
+    sudo python3 setup.py develop
+    sleep 1
+    echo ""
+    echo ""
+fi
+
+
+
+# SAND OPT 3RPC
+if [ $option = "opt3RPC" ];
+then
+gegede-cli duneggd/Config/JUST_EMI/WORLDggd.cfg \
+           duneggd/Config/JUST_EMI/ND_Hall_Air_Volume.cfg \
+           duneggd/Config/JUST_EMI/ND_Hall_Rock.cfg \
+           duneggd/Config/SAND_MAGNET.cfg \
+           duneggd/Config/SAND_ECAL.cfg \
+           duneggd/Config/SAND_EMI_RPC.cfg \
+           -w World -o ${outdir}/SAND_opt3RPC.gdml
+            read -r -p "Do you want to display \"${outdir}/SAND_opt3RPC.gdml\"? [y/N] " response
+            case "$response" in
+                [yY][eE][sS]|[yY]) 
+                    echo "./geodisplay_gdml \"${outdir}/SAND_opt3RPC.gdml)\""; 
+                    ./geodisplay_gdml "${outdir}/SAND_opt3RPC.gdml"; 
+                    ;;
+                *)
+                    do_something_else
+                    ;;
+            esac
+fi
+
+
+
+
+
+
+
+# SAND OPT 2
+if [ $option = "all" -o $option = "sand_opt2" ];
+then
+    echo "You have chosen to build the ND without EMI!"
+    echo "Building it..."
+gegede-cli duneggd/Config/WORLDggd.cfg \
+           duneggd/Config/ND_Hall_Air_Volume.cfg \
+           duneggd/Config/ND_Hall_Rock.cfg \
+           duneggd/Config/ND_ElevatorStruct.cfg \
+           duneggd/Config/SAND_MAGNET.cfg \
+           duneggd/Config/SAND_INNERVOLOPT2.cfg \
+           duneggd/Config/SAND_ECAL.cfg \
+           duneggd/Config/SAND_STT.cfg \
+           duneggd/Config/SAND_GRAIN.cfg \
+           duneggd/Config/ND_CraneRailStruct1.cfg \
+           duneggd/Config/ND_CraneRailStruct2.cfg \
+           duneggd/Config/ND_HallwayStruct.cfg \
+           duneggd/Config/ND_CryoStruct.cfg \
+           duneggd/Config/ND-GAr/ND-GAr-SPYv3_noTPC.cfg \
+           duneggd/Config/ND-GAr/ND-GAr-SPYv3.cfg \
+           duneggd/Config/ArgonCube/ArgonCubeCryostat.cfg \
+           duneggd/Config/ArgonCube/ArgonCubeDetector.cfg \
+           -w World -o SAND_opt2.gdml
+fi
+
+
+
+
+# SAND OPT 3  [opt2 + EMI]
+if [ $option = "all" -o $option = "sand_opt3" ];
+then
+echo "You have chosen to build the ND with scintillator-EMI!"
+echo "Building it..."
+gegede-cli duneggd/Config/WORLDggd.cfg \
+           duneggd/Config/ND_Hall_Air_Volume.cfg \
+           duneggd/Config/ND_Hall_Rock.cfg \
+           duneggd/Config/ND_ElevatorStruct.cfg \
+           duneggd/Config/SAND_MAGNET.cfg \
+           duneggd/Config/SAND_INNERVOLOPT2.cfg \
+           duneggd/Config/SAND_EMI.cfg \
+           duneggd/Config/SAND_ECAL.cfg \
+           duneggd/Config/SAND_STT.cfg \
+           duneggd/Config/SAND_GRAIN.cfg \
+           duneggd/Config/ND_CraneRailStruct1.cfg \
+           duneggd/Config/ND_CraneRailStruct2.cfg \
+           duneggd/Config/ND_HallwayStruct.cfg \
+           duneggd/Config/ND_CryoStruct.cfg \
+           duneggd/Config/ND-GAr/ND-GAr-SPYv3_noTPC.cfg \
+           duneggd/Config/ND-GAr/ND-GAr-SPYv3.cfg \
+           duneggd/Config/ArgonCube/ArgonCubeCryostat.cfg \
+           duneggd/Config/ArgonCube/ArgonCubeDetector.cfg \
+           -w World -o ${outdir}/SAND_opt3.gdml
+            read -r -p "Do you want to display \"${outdir}/SAND_opt3.gdml\"? [y/N] " response
+            case "$response" in
+                [yY][eE][sS]|[yY]) 
+                    echo "./geodisplay_gdml \"${outdir}/SAND_opt3.gdml)\""; 
+                    ./geodisplay_gdml "${outdir}/SAND_opt3.gdml"; 
+                    ;;
+                *)
+                    do_something_else
+                    ;;
+            esac
+fi
+
+
+# SAND OPT 4  [opt2 + RPC-EMI]
+           #duneggd/Config/JUST_EMI/SAND_STT.cfg \
+           #duneggd/Config/JUST_EMI/SAND_GRAIN.cfg \
+           #duneggd/Config/SAND_INNERVOLOPT2.cfg \
+           #duneggd/Config/JUST_EMI/SAND_ECAL.cfg \
+if [ $option = "justemi" ];
+then
+gegede-cli duneggd/Config/JUST_EMI/WORLDggd.cfg \
+           duneggd/Config/JUST_EMI/ND_Hall_Air_Volume.cfg \
+           duneggd/Config/JUST_EMI/ND_Hall_Rock.cfg \
+           duneggd/Config/JUST_EMI/SAND_MAGNET.cfg \
+           duneggd/Config/JUST_EMI/SAND_ECAL.cfg \
+           duneggd/Config/JUST_EMI/SAND_EMI_RPC.cfg \
+           -w World -o ${outdir}/SAND_JUST_EMI_RPC.gdml
+            read -r -p "Do you want to display \"${outdir}/SAND_JUST_EMI_RPC.gdml\"? [y/N] " response
+            case "$response" in
+                [yY][eE][sS]|[yY]) 
+                    echo "./geodisplay_gdml \"${outdir}/SAND_JUST_EMI_RPC.gdml)\""; 
+                    ./geodisplay_gdml "${outdir}/SAND_JUST_EMI_RPC.gdml"; 
+                    ;;
+                *)
+                    do_something_else
+                    ;;
+            esac
+fi
+
+
+
+
+
+
+# KLOE anti-fiducial
+if [ $option = "all" -o $option = "kloe_antifid" ];
+then
+gegede-cli duneggd/Config/WORLDggd.cfg \
+	   duneggd/Config/ND_Hall_Air_Volume_Only_KLOE.cfg \
+	   duneggd/Config/ND_Hall_Rock.cfg \
+	   duneggd/Config/ND_ElevatorStruct.cfg \
+	   duneggd/Config/KLOE_No_3DST.cfg \
+	   duneggd/Config/KLOEEMCALO.cfg \
+	   duneggd/Config/ND-GAr/ND-GAr-SPYv3.cfg \
+	   duneggd/Config/ND_CraneRailStruct1.cfg \
+	   duneggd/Config/ND_CraneRailStruct2.cfg \
+	   duneggd/Config/ND_HallwayStruct.cfg \
+	   duneggd/Config/ND_CryoStruct.cfg \
+	   duneggd/Config/ArgonCube/ArgonCubeCryostat.cfg \
+	   duneggd/Config/ArgonCube/ArgonCubeDetector.cfg \
+	   -w World -o nd_hall_only_kloe_antifid.gdml
+fi
+
+# LAr and MPD (No KLOE)
+if [ $option = "all" -o $option = "lar_mpd" ];
+then
+gegede-cli duneggd/Config/WORLDggd.cfg \
+	   duneggd/Config/ND_Hall_Air_Volume_No_KLOE.cfg \
+	   duneggd/Config/ND_Hall_Rock.cfg \
+	   duneggd/Config/ND_ElevatorStruct.cfg \
+	   duneggd/Config/KLOE_with_3DST.cfg \
+	   duneggd/Config/KLOEEMCALO.cfg \
+	   duneggd/Config/ND-GAr/ND-GAr-SPYv3.cfg \
+	   duneggd/Config/ND_CraneRailStruct1.cfg \
+	   duneggd/Config/ND_CraneRailStruct2.cfg \
+	   duneggd/Config/ND_HallwayStruct.cfg \
+	   duneggd/Config/ND_CryoStruct.cfg \
+	   duneggd/Config/ArgonCube/ArgonCubeCryostat.cfg \
+	   duneggd/Config/ArgonCube/ArgonCubeDetector.cfg \
+	   -w World -o nd_hall_lar_mpd.gdml
+fi
+
+# LAr and MPD anti-fiducial (for MPD)
+if [ $option = "all" -o $option = "lar_mpd_antifid" ];
+then
+gegede-cli duneggd/Config/WORLDggd.cfg \
+	   duneggd/Config/ND_Hall_Air_Volume_No_KLOE.cfg \
+	   duneggd/Config/ND_Hall_Rock.cfg \
+	   duneggd/Config/ND_ElevatorStruct.cfg \
+	   duneggd/Config/KLOE_with_3DST.cfg \
+	   duneggd/Config/KLOEEMCALO.cfg \
+	   duneggd/Config/ND-GAr/ND-GAr-SPYv3_noTPC.cfg \
+	   duneggd/Config/ND_CraneRailStruct1.cfg \
+	   duneggd/Config/ND_CraneRailStruct2.cfg \
+	   duneggd/Config/ND_HallwayStruct.cfg \
+	   duneggd/Config/ND_CryoStruct.cfg \
+	   duneggd/Config/ArgonCube/ArgonCubeCryostat.cfg \
+	   duneggd/Config/ArgonCube/ArgonCubeDetector.cfg \
+	   -w World -o nd_hall_lar_mpd_antifid.gdml
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if [ $option = "all" -o $option = "miniproduction1_gar" ];
@@ -325,126 +533,7 @@ gegede-cli duneggd/Config/WORLDggd.cfg \
            -w World -o SAND_opt1.gdml
 fi
 
-# SAND OPT 2
-if [ $option = "all" -o $option = "sand_opt2" ];
-then
-gegede-cli duneggd/Config/WORLDggd.cfg \
-           duneggd/Config/ND_Hall_Air_Volume.cfg \
-           duneggd/Config/ND_Hall_Rock.cfg \
-           duneggd/Config/ND_ElevatorStruct.cfg \
-           duneggd/Config/SAND_MAGNET.cfg \
-           duneggd/Config/SAND_INNERVOLOPT2.cfg \
-           duneggd/Config/SAND_ECAL.cfg \
-           duneggd/Config/SAND_STT.cfg \
-           duneggd/Config/SAND_GRAIN.cfg \
-           duneggd/Config/ND_CraneRailStruct1.cfg \
-           duneggd/Config/ND_CraneRailStruct2.cfg \
-           duneggd/Config/ND_HallwayStruct.cfg \
-           duneggd/Config/ND_CryoStruct.cfg \
-           duneggd/Config/ND-GAr/ND-GAr-SPYv3_noTPC.cfg \
-           duneggd/Config/ND-GAr/ND-GAr-SPYv3.cfg \
-           duneggd/Config/ArgonCube/ArgonCubeCryostat.cfg \
-           duneggd/Config/ArgonCube/ArgonCubeDetector.cfg \
-           -w World -o SAND_opt2.gdml
-fi
 
-
-
-
-# SAND OPT 3  [opt2 + EMI]
-if [ $option = "all" -o $option = "sand_opt3" ];
-then
-gegede-cli duneggd/Config/WORLDggd.cfg \
-           duneggd/Config/ND_Hall_Air_Volume.cfg \
-           duneggd/Config/ND_Hall_Rock.cfg \
-           duneggd/Config/ND_ElevatorStruct.cfg \
-           duneggd/Config/SAND_MAGNET.cfg \
-           duneggd/Config/SAND_INNERVOLOPT2.cfg \
-           duneggd/Config/SAND_EMI.cfg \
-           duneggd/Config/SAND_ECAL.cfg \
-           duneggd/Config/SAND_STT.cfg \
-           duneggd/Config/SAND_GRAIN.cfg \
-           duneggd/Config/ND_CraneRailStruct1.cfg \
-           duneggd/Config/ND_CraneRailStruct2.cfg \
-           duneggd/Config/ND_HallwayStruct.cfg \
-           duneggd/Config/ND_CryoStruct.cfg \
-           duneggd/Config/ND-GAr/ND-GAr-SPYv3_noTPC.cfg \
-           duneggd/Config/ND-GAr/ND-GAr-SPYv3.cfg \
-           duneggd/Config/ArgonCube/ArgonCubeCryostat.cfg \
-           duneggd/Config/ArgonCube/ArgonCubeDetector.cfg \
-           -w World -o ${outdir}/SAND_opt3.gdml
-            read -r -p "Do you want to display \"${outdir}/SAND_opt3.gdml\"? [y/N] " response
-            case "$response" in
-                [yY][eE][sS]|[yY]) 
-                    cd ROOT_MACROS/BIN/;
-                    echo "./geodisplay_gdml \"${outdir}/SAND_opt3.gdml)\""; 
-                    ./geodisplay_gdml "${outdir}/SAND_opt3.gdml"; 
-                    cd $curdir;
-                    ;;
-                *)
-                    do_something_else
-                    ;;
-            esac
-fi
-
-
-
-# KLOE anti-fiducial
-if [ $option = "all" -o $option = "kloe_antifid" ];
-then
-gegede-cli duneggd/Config/WORLDggd.cfg \
-	   duneggd/Config/ND_Hall_Air_Volume_Only_KLOE.cfg \
-	   duneggd/Config/ND_Hall_Rock.cfg \
-	   duneggd/Config/ND_ElevatorStruct.cfg \
-	   duneggd/Config/KLOE_No_3DST.cfg \
-	   duneggd/Config/KLOEEMCALO.cfg \
-	   duneggd/Config/ND-GAr/ND-GAr-SPYv3.cfg \
-	   duneggd/Config/ND_CraneRailStruct1.cfg \
-	   duneggd/Config/ND_CraneRailStruct2.cfg \
-	   duneggd/Config/ND_HallwayStruct.cfg \
-	   duneggd/Config/ND_CryoStruct.cfg \
-	   duneggd/Config/ArgonCube/ArgonCubeCryostat.cfg \
-	   duneggd/Config/ArgonCube/ArgonCubeDetector.cfg \
-	   -w World -o nd_hall_only_kloe_antifid.gdml
-fi
-
-# LAr and MPD (No KLOE)
-if [ $option = "all" -o $option = "lar_mpd" ];
-then
-gegede-cli duneggd/Config/WORLDggd.cfg \
-	   duneggd/Config/ND_Hall_Air_Volume_No_KLOE.cfg \
-	   duneggd/Config/ND_Hall_Rock.cfg \
-	   duneggd/Config/ND_ElevatorStruct.cfg \
-	   duneggd/Config/KLOE_with_3DST.cfg \
-	   duneggd/Config/KLOEEMCALO.cfg \
-	   duneggd/Config/ND-GAr/ND-GAr-SPYv3.cfg \
-	   duneggd/Config/ND_CraneRailStruct1.cfg \
-	   duneggd/Config/ND_CraneRailStruct2.cfg \
-	   duneggd/Config/ND_HallwayStruct.cfg \
-	   duneggd/Config/ND_CryoStruct.cfg \
-	   duneggd/Config/ArgonCube/ArgonCubeCryostat.cfg \
-	   duneggd/Config/ArgonCube/ArgonCubeDetector.cfg \
-	   -w World -o nd_hall_lar_mpd.gdml
-fi
-
-# LAr and MPD anti-fiducial (for MPD)
-if [ $option = "all" -o $option = "lar_mpd_antifid" ];
-then
-gegede-cli duneggd/Config/WORLDggd.cfg \
-	   duneggd/Config/ND_Hall_Air_Volume_No_KLOE.cfg \
-	   duneggd/Config/ND_Hall_Rock.cfg \
-	   duneggd/Config/ND_ElevatorStruct.cfg \
-	   duneggd/Config/KLOE_with_3DST.cfg \
-	   duneggd/Config/KLOEEMCALO.cfg \
-	   duneggd/Config/ND-GAr/ND-GAr-SPYv3_noTPC.cfg \
-	   duneggd/Config/ND_CraneRailStruct1.cfg \
-	   duneggd/Config/ND_CraneRailStruct2.cfg \
-	   duneggd/Config/ND_HallwayStruct.cfg \
-	   duneggd/Config/ND_CryoStruct.cfg \
-	   duneggd/Config/ArgonCube/ArgonCubeCryostat.cfg \
-	   duneggd/Config/ArgonCube/ArgonCubeDetector.cfg \
-	   -w World -o nd_hall_lar_mpd_antifid.gdml
-fi
 
 # LAr and TMS (No KLOE)
 if [ $option = "all" -o $option = "lar_tms" ];
@@ -466,27 +555,3 @@ gegede-cli duneggd/Config/WORLDggd.cfg \
 fi
 
 
-
-
-# SAND OPT 3  [opt2 + EMI + all AIR]
-if [ $option = "justemi" ];
-then
-gegede-cli duneggd/Config/JUST_EMI/WORLDggd.cfg \
-           duneggd/Config/JUST_EMI/ND_Hall_Air_Volume.cfg \
-           duneggd/Config/JUST_EMI/ND_Hall_Rock.cfg \
-           duneggd/Config/JUST_EMI/SAND_MAGNET.cfg \
-           duneggd/Config/JUST_EMI/SAND_EMI.cfg \
-           -w World -o ${outdir}/SAND_JUST_EMI.gdml
-            read -r -p "Do you want to display \"${outdir}/SAND_JUST_EMI.gdml\"? [y/N] " response
-            case "$response" in
-                [yY][eE][sS]|[yY]) 
-                    cd ROOT_MACROS/BIN/;
-                    echo "./geodisplay_gdml \"${outdir}/SAND_JUST_EMI.gdml)\""; 
-                    ./geodisplay_gdml "${outdir}/SAND_JUST_EMI.gdml"; 
-                    cd $curdir;
-                    ;;
-                *)
-                    do_something_else
-                    ;;
-            esac
-fi
