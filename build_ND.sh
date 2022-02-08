bd86a02c9b1af829959a44813bce59086c5a66cf
quantatanu <atanu.quanta@gmail.com>
2022-02-08 09:26:05 +0530

    staggered emi rpc final design 2 layers, all cfgs added, full ND hall

--- build_ND.sh
+++ build_ND.sh
@@ -40,6 +40,57 @@ main (){
         gegede-cli duneggd/Config/WORLDggd.cfg \
                    duneggd/Config/ND_Hall_Air_Volume.cfg \
                    duneggd/Config/ND_Hall_Rock.cfg \
+                   duneggd/Config/ND_ElevatorStruct.cfg \
+                   duneggd/Config/SAND_MAGNET.cfg \
+                   duneggd/Config/SAND_INNERVOLOPT2.cfg \
+                   duneggd/Config/SAND_EMI_RPC.cfg \
+                   duneggd/Config/SAND_ECAL.cfg \
+                   duneggd/Config/SAND_STT.cfg \
+                   duneggd/Config/SAND_GRAIN.cfg \
+                   duneggd/Config/ND_CraneRailStruct1.cfg \
+                   duneggd/Config/ND_CraneRailStruct2.cfg \
+                   duneggd/Config/ND_HallwayStruct.cfg \
+                   duneggd/Config/ND_CryoStruct.cfg \
+                   duneggd/Config/ND-GAr/ND-GAr-SPYv3_noTPC.cfg \
+                   duneggd/Config/ND-GAr/ND-GAr-SPYv3.cfg \
+                   duneggd/Config/ArgonCube/ArgonCubeCryostat.cfg \
+                   duneggd/Config/ArgonCube/ArgonCubeDetector.cfg \
+                   -w World -o $output
+                read -r -p "Do you want to display \"$output\"? [y/N] " response
+                case "$response" in
+                    [yY][eE][sS]|[yY]) 
+                        echo "./geodisplay_gdml \"$output\""; 
+                        ./geodisplay_gdml "$output"; 
+                        ;;
+                    *)
+                        echo "Exiting...";
+                        ;;
+                esac
+                read -r -p "Do you want to delete the file: \"$output\"? [y/N] " response
+                case "$response" in
+                    [yY][eE][sS]|[yY]) 
+                        echo "deleting \"$output\" ..."; 
+                        echo "rm -rf \"$output\""; 
+                        rm -rf "$output"; 
+                        ;;
+                    *)
+                        echo "";
+                        ;;
+                esac
+
+    fi
+
+
+
+
+
+    # SAND OPT 3RPC test
+    if [[ "${@}" == *"opt3RPC_test"* ]];
+    then
+        output="${outdir}/SAND_opt3RPC_test${tags}.gdml"
+        gegede-cli duneggd/Config/WORLDggd.cfg \
+                   duneggd/Config/ND_Hall_Air_Volume.cfg \
+                   duneggd/Config/ND_Hall_Rock.cfg \
                    duneggd/Config/SAND_MAGNET.cfg \
                    duneggd/Config/SAND_ECAL.cfg \
                    duneggd/Config/SAND_EMI_RPC.cfg \
