b01ae690bf2d2fc9d7b684ad9aa237daea24bad3
quantatanu <atanu.quanta@gmail.com>
2022-02-14 23:07:39 +0530

    modified build_ND.sh..

---
+++ test_build_ND.sh
@@ -0,0 +1,82 @@
+#! /bin/bash
+
+
+# example run:
+# ./build_ND.sh opt3RPC -C -O $(date +"%Y%m%d%H%M%S")       # where -C invokes a compilation and whatever comes after -O is added as a tag to the output gdml file name
+
+
+curdir=$(pwd);
+outdir="${curdir}/OUTPUT/GDML/";
+
+
+
+main (){
+
+
+
+    time_stamp=$(date +"%Y%m%d")
+
+    reset;
+    echo "---------------------------------------"
+    if [[ "${@}" == *"-O"* ]]
+    then
+        tags=$(echo "${@}" | awk -F '-O' '{print $2}' | sed 's/\ /_/g');
+    else 
+        tags="";
+    fi
+
+    if [[ "${@}" == *"-C"* ]] || [[ "${@}" == *"-compile"* ]]
+    then
+        echo "You have chosen to compile, compiling...."
+        sudo python3 setup.py develop
+        echo ""
+        echo ""
+    fi
+
+
+
+
+
+
+
+
+    # SAND OPT 3RPC test
+    if [[ "${@}" == *"opt3RPC_test"* ]];
+    then
+        output="${outdir}/SAND_opt3RPC_test_${time_stamp}${tags}.gdml"
+        gegede-cli duneggd/Config/TEST/WORLDggd.cfg \
+                   duneggd/Config/TEST/ND_Hall_Air_Volume.cfg \
+                   duneggd/Config/TEST/ND_Hall_Rock.cfg \
+                   duneggd/Config/TEST/SAND_MAGNET.cfg \
+                   duneggd/Config/TEST/SAND_ECAL.cfg \
+                   duneggd/Config/TEST/SAND_EMI_RPC.cfg \
+                   -w World -o $output
+                read -r -p "Do you want to display \"$output\"? [y/N] " response
+                case "$response" in
+                    [yY][eE][sS]|[yY]) 
+                        echo "./geodisplay_gdml \"$output\""; 
+                        ./geodisplay_gdml "$output"; 
+                        ;;
+                    *)
+                        echo "Exiting...";
+                        ;;
+                esac
+                read -r -p "Do you want to delete the file: \"$output\"? [y/N] " response
+                case "$response" in
+                    [yY][eE][sS]|[yY]) 
+                        echo "deleting \"$output\" ..."; 
+                        echo "rm -rf \"$output\""; 
+                        rm -rf "$output"; 
+                        ;;
+                    *)
+                        echo "";
+                        ;;
+                esac
+
+    fi
+
+
+}
+
+
+main "$@"
